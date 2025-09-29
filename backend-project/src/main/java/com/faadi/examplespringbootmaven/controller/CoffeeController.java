3489bf6c0c8ee85184d34b0bba10e2c91a0e98ee
mfahadqureshi786 <mfahadqureshi786@gmail.com>
2025-09-30 00:22:16 +0200

    updated changes

--- backend-project/src/main/java/com/faadi/examplespringbootmaven/controller/CoffeeController.java
+++ backend-project/src/main/java/com/faadi/examplespringbootmaven/controller/CoffeeController.java
@@ -1,14 +1,11 @@
 package com.faadi.examplespringbootmaven.controller;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.faadi.examplespringbootmaven.entity.Coffee;
